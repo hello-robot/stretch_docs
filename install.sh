09669bf90007052e7264dcdac655f2705351997e
Aaron Edsinger <aedsinger@hello-robot.com>
2021-06-19 21:22:19 -0700

    initial site refactor done

---
+++ install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+pip3 install mkdocs --no-cache-dir
+pip3 install mkdocs-material --no-cache-dir
+pip3 install mkdocs-simple-plugin --no-cache-dir
+pip3 install mktheapidocs --no-cache-dir
+
+
